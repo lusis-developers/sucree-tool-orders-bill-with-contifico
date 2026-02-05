af920335bb3fa7859135451d68601bde66da4d21
Diego Reyes <diegorele13@gmail.com>
2026-02-05 12:36:30 -0500

    feat(models): add DeliveryPersonModel to model exports
    Include DeliveryPersonModel in the centralized models export to make it accessible throughout the application.

--- src/models/index.ts
+++ src/models/index.ts
@@ -2,10 +2,12 @@ import { OrderModel } from "./order.model";
 import { DailySummaryModel } from "./daily-summary.model";
 import { UserModel } from "./user.model";
 import { ParLevelModel } from "./par-level.model";
+import { DeliveryPersonModel } from "./delivery-person.model";
 
 export const models = {
   orders: OrderModel,
   dailySummaries: DailySummaryModel,
   users: UserModel,
   parLevels: ParLevelModel,
+  deliveryPersons: DeliveryPersonModel,
 };
